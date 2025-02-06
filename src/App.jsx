8abacbe92df2d9100f834fbf2700855a2bb4f455
Ahrorbek05 <akramjonovahrorbek96@gmail.com>
2025-02-06 23:17:53 +0500

    auth remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Sidebar from './components/sidebar/Sidebar';
 import Students from './pages/Students';
@@ -13,31 +13,30 @@ import Login from './pages/Login';
 const App = () => {
   const [loading, setLoading] = useState(false);
   const location = useLocation();
-  const isAuthenticated = localStorage.getItem("accessToken");
 
   useEffect(() => {
     setLoading(true);
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1000);
-
     return () => clearTimeout(timer);
   }, [location]);
 
   return (
     <div className="flex">
-      {isAuthenticated && <Sidebar />}
+      {/* Sidebar har doim ko'rinadi */}
+      <Sidebar />
       <div className="flex-grow">
         {loading ? (
           <Loader />
         ) : (
           <Routes>
-            <Route path="/" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
-            <Route path="/students" element={isAuthenticated ? <Students /> : <Navigate to="/login" />} />
-            <Route path="/courses" element={isAuthenticated ? <VideoCourses /> : <Navigate to="/login" />} />
-            <Route path='/textbooks' element={isAuthenticated ? <VideoSection /> : <Navigate to="/login" />} />
-            <Route path='/register' element={!isAuthenticated ? <Register /> : <Navigate to="/" />} />
-            <Route path='/login' element={!isAuthenticated ? <Login /> : <Navigate to="/" />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/courses" element={<VideoCourses />} />
+            <Route path="/textbooks" element={<VideoSection />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         )}
       </div>
@@ -46,54 +45,3 @@ const App = () => {
 };
 
 export default App;
-
-
-// //**** Authetifikatsiya olib tashlangan ****// //
-
-// import { useState, useEffect } from 'react';
-// import { Routes, Route, useLocation } from 'react-router-dom';
-// import Dashboard from './pages/Dashboard';
-// import Sidebar from './components/sidebar/Sidebar';
-// import Students from './pages/Students';
-// import VideoCourses from './pages/Courses';
-// import VideoSection from './components/VideoSection/VideoSection';
-// import Loader from './components/loader/Loader';
-// import Register from './pages/Register';
-// import Login from './pages/Login';
-// import './App.css';
-
-// const App = () => {
-//   const [loading, setLoading] = useState(false);
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     setLoading(true);
-//     const timer = setTimeout(() => {
-//       setLoading(false);
-//     }, 1000);
-
-//     return () => clearTimeout(timer);
-//   }, [location]);
-
-//   return (
-//     <div className="flex">
-//       <Sidebar />
-//       <div className="flex-grow">
-//         {loading ? (
-//           <Loader />
-//         ) : (
-//           <Routes>
-//             <Route path="/" element={<Dashboard />} />
-//             <Route path="/students" element={<Students />} />
-//             <Route path="/courses" element={<VideoCourses />} />
-//             <Route path="/textbooks" element={<VideoSection />} />
-//             <Route path="/register" element={<Register />} />
-//             <Route path="/login" element={<Login />} />
-//           </Routes>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
